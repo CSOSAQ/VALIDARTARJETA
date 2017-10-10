dbb904bb236a574a288cf961b92a34ae829f9926
CSOSAQ <csosaquispe@gmail.com>
2017-10-10 17:57:33 -0500

    Proyecto validar tarjeta

---
+++ README.md
@@ -0,0 +1,19 @@
+## PROYECTO: Validar Tarjeta
+Validar tarjeta es un sistema que permite validar el número de una tarjeta.
+### Pseudocódigo
+
+#####Input:
+    - El programa permitirá ingresar un número de 13 dígitos
+
+#####Process: 
+- Encriptar:
+    - El programa leerá el número.
+    - El programa validará que el valor ingresado se número y no sea vacio y sea de 13 dígitos.
+    - El programa revertirá la posición de los números.
+    - El programa multiplicará los números de posiciones pares.
+    - El programa si los números multiplicados son mayor o igual a 10, sumará sus dígitos
+    - El programa sumará todos los números y sacará su módulo de 10
+    - El programa validará si el resultado del módulo sale 0; es valido; sino es inválido
+
+### Diagrama de Flujo
+[DiagramaFlujo](file:///C:/Users/CSOSA/Desktop\Repositorio-1\CIPHERCESAR\assets/DF.svg)
---
+++ js/app.js
@@ -0,0 +1,45 @@
+var numbertarjet= prompt("Ingrese el número de su tarjeta");
+function isValidCrad(numbertarjet){
+    var newarr =[];
+    var newarru=[];
+    var newarrmu=[];
+    var sum =0;
+    var sumimp=0;
+   
+    for(i=0; i< numbertarjet.length; i++){
+       newarr.push(numbertarjet[i]);   
+    }
+    newarr.reverse();
+
+   // console.log(newarr); 3,2,1
+    for(j=1;j< newarr.length;j=j+2){ 
+        var mulpar= newarr[j]*2 ;
+        if(mulpar >=10){
+            var cadena =mulpar.toString();
+            cadena.split("");
+            for(k=0; k< cadena.length;k++)
+            
+            sum= parseInt(cadena[k]) +sum;
+            
+        }
+        else{
+            newarr[j]=mulpar;
+           sum = newarr[j] + sum
+        }
+    }
+    for(l=0; l<newarr.length;l=l+2){
+     
+       var sumimp= sumimp + parseInt(newarr[l]);
+
+    }
+       
+
+    var validar=(sumimp + sum)%10
+    if(validar ==0){
+    alert("El número de tarjeta es valido")
+}
+else{
+    alert("El número de tarjeta es invalido")
+}
+}
+isValidCrad(numbertarjet);
